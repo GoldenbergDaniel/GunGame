15fe5cce376a44352479ee1b28a1b6ac37a97e5e
GoldenbergDaniel <danielgoldenberg101@gmail.com>
2022-08-15 13:01:07 -0400

    Added windows support and changes

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,6 +1,19 @@
 extends Node
 
 
+func _ready() -> void:
+	var os_name = OS.get_name()
+	var screen_size = OS.get_screen_size()
+	var window_size = OS.get_window_size()
+
+	if os_name == "OSX":
+		OS.window_size = Vector2(1920, 1080)
+	elif os_name == "Windows":
+		OS.window_size = Vector2(1280, 720)
+
+	OS.window_position = (screen_size*0.5 - window_size*0.5)
+
+
 func _process(_delta) -> void:
 	if Input.is_action_pressed("ui_exit"):
 		get_tree().quit()
